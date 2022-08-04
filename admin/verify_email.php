69543c3c2da10462d3c7c92dbfa4b4ebede14f0c
Severin Rutayisire <severinmcevorin1@outlook.com>
2022-08-04 15:54:21 +0200

    changes

---
+++ admin/verify_email.php
@@ -0,0 +1,29 @@
+<?php 
+include_once 'config/Database.php';
+include_once 'class/subscription.php';
+
+$database = new Database();
+$db = $database->getConnection();
+$subscriber = new Subscription($db);
+
+$verifyMessage = '';
+if(!empty($_GET['email_verify'])){     
+	$token = $_GET['email_verify']; 	
+	$subscriber->verify_token = $token;
+    if($subscriber->isValidToken()){ 
+       	$subscriber->is_verified = 1;        
+        if($subscriber->update()) { 
+            $verifyMessage = '<p class="success">Your email address has been verified successfully.</p>'; 
+        } else { 
+            $verifyMessage = '<p class="error">Some problem occurred on verifying your email, please try again.</p>'; 
+        } 
+    } else { 
+        $verifyMessage = '<p class="error">You have clicked on the wrong link, please check your email and try again.</p>'; 
+	}
+}
+include('inc/header.php');
+?>
+
+<?php include('template/home-footer.php');?>
+
+
