a382f9de7349ce6dd57df6be159cf65c7b0bde04
Severin Rutayisire <severinmcevorin1@outlook.com>
2022-08-08 20:06:34 +0200

    changes

--- challenges.php
+++ challenges.php
@@ -66,20 +66,126 @@
 
                     <div class="row">
                         <div class="col-md-8 offset-md-2">
+                        <h6 class="section-sub-title mb-20">About the Challenge</h6>
                             <p>
-                                The Novartis Foundation, in partnership with Norrsken East Africa and the Global Fund, is pleased to announce the 2023 HealthTech Challenge!
+                            <b>Norrsken East Africa</b> in collaboration with the <b>Novartis Foundation</b> and <b>the Global Fund</b>, organize the Africa HealthTech Challenge every year!
+
+The HealthTech Challenge is an annual innovation-led program that brings together startups from across the health ecosystem to innovate, collaborate and ultimately create better healthcare experiences for all people. Since its inception, the HealthTech Hub’s Challenge has brought healthcare organizations and startups together to tackle the world’s most significant health issues. Supported by the Novartis Foundation, in collaboration with the Norrsken Foundation, the initial focus is on technologies helping fight against heart disease and breast cancer and developing virtual health and care.
+
                             </p>
 
+                            <p style="color:blue"><b>
+                            The challenge sources for the best high-impact innovation potentials with revolutionary business-to-government focused solutions and business models driving population health impact at scale for Africa.</p></b>
                             <p>
-                                The HealthTech Challenge is an annual innovation-led program that brings together startups from across the health ecosystem to innovate, collaborate and ultimately create better healthcare experiences for all people. Since its inception, the HealthTech Hub’s Challenge has brought healthcare organizations and startups together to tackle the world’s most significant health issues. Supported by the Novartis Foundation, in collaboration with the Norrsken Foundation, the initial focus is on technologies helping fight against heart disease and breast cancer and developing virtual health and care.</p>
-                            <p>
-                                All selected startups benefit from a full year of access to a state-of-the-art co-working space, take part in a 10-month health tech acceleration program that includes training, expert coaching, and mentorship, and get support from public and private partners, including a number of benefits, match-making activities, and opportunities to test, validate and integrate their solutions in public health systems.
+                            For Year 2, starting from January 1st 2023, the Global Fund joins the Novartis Foundation and Norrsken East Africa to collaborate with the HealthTech Hub Africa to contribute towards the above objectives and also expand towards building local private sector capacity for data science and digital health, strengthening community health information systems, and laying the foundations for the scale-up of both new and existing solutions which strengthen health information systems more broadly. 
+
+The 2023 Challenge will welcome startup applications from any African country. It will focus on four thematic areas and impact goals for the public health systems of Rwanda, Senegal, Uganda, Ethiopia, Burkina Faso:
                             </p>
+
+                            <ul>
+                                <li>Cardiovascular health</li>
+                                <li>Breast cancer</li>
+                                <li>Virtual health and care</li>
+                                <li>Optimization of data-driven decision-making</li>
+                            </ul>
+                                <br>
+                                  <h6 class="section-sub-title mb-20">Why you should apply ?</h6>
+                                    <b><p>The selected forty startups will benefit from:</p></b>
+                                      <div class="content">
+                                        <div class="text">
+                                                <ul class="benefits" style="">
+                                                     <ol type="1">
+                                                        <li>A seat in the 10-month innovation program with 2 distinct tracks for the Growth Startups and the Scale ups</li>
+                                                        <li>Opportunity to apply for funding to validate their solutions in public health systems in AfricaAccess to the HealthTech Hub Africa public-private partner network</li>
+                                                        <li>A spot in the one-year coworking space in Kigali, Rwanda. </li>
+                                                        <li>Exposure and partner match-making opportunities</li>
+                                                        <li>Life-time access to the alumni network of HealthTech Hub Africa startups and open-source resources</li>
+                                                    </ol>
+                                                    </ul><br>
+                                                    <h6 class="section-sub-title mb-20">Eligibility Criteria</h6>
+                                                    <b><p>The selected forty startups will benefit from:</p></b>
+                                      <div class="content">
+                                        <div class="text">
+                                                <ul class="benefits" style="">
+                                                     <ol type="1">
+                                                        <li>Evidence of a legal entity registered in an African country
+</li>
+                                                        <li>Clearly demonstrated population health impact through their solution</li>
+                                                        <li>Teams of two or more people can apply, and those with a more positive gender balance will be prioritized. </li>
+                                                        <li>Startups must focus on at least one of the four thematic areas of the Hub: cardiovascular health, breast cancer, virtual health and care, and optimization of data-driven decision-making
+</li>
+                                                        <li>Startups are not part of another accelerator program in the same year if they are selected</li>
+                                                    </ol>
+                                                    </ul><br>
+                                                    <b><p>There are two application tracks for which organizations can apply only once a year:
+</p></b><br>
+<div class="col-lg-8"> </div>
+
+                                                    <div class="grid-item animate">
+                                                        <div class="ht-box-icon style-03">
+                                                            <div class="icon-box-wrap">
+                                                                <div class="content-header">
+                                                                    <!-- <div class="icon">
+                                                                        <i class="fal fa-magnet"></i>
+                                                                    </div> -->
+                                                                    <h6 class="heading">
+                                                                    Growth startups additional eligibility criteria and conditions:</h6>
+                                                                </div>
+                                                                <div class="content">
+                                                                    <div class="text">
+                                                                        <ul class="benefits" style="">
+                                                                            <ol type="1">
+                                                                                <li>A seat in the 10-month innovation program with 2 distinct tracks for the Growth Startups and the Scale ups</li>
+                                                                                <li>Opportunity to apply for funding to validate their solutions in public health systems in AfricaAccess to the HealthTech Hub Africa public-private partner network</li>
+                                                                                <li>A spot in the one-year coworking space in Kigali, Rwanda. </li>
+                                                                                <li>Exposure and partner match-making opportunities</li>
+                                                                                <li>Life-time access to the alumni network of HealthTech Hub Africa startups and open-source resources</li>
+                                                                            </ol>
+                                                                        </ul>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div><br>
+                                                <div class="grid-item animate">
+                                                        <div class="ht-box-icon style-03">
+                                                            <div class="icon-box-wrap">
+                                                                <div class="content-header">
+                                                                    <!-- <div class="icon">
+                                                                        <i class="fal fa-magnet"></i>
+                                                                    </div> -->
+                                                                    <h6 class="heading">
+                                                                    Scale ups additional evaluation criteria and conditions:
+</h6>
+                                                                </div>
+                                                                <div class="content">
+                                                                    <div class="text">
+                                                                        <ul class="benefits" style="">
+                                                                            <ol type="1">
+                                                                                <li>Solution must be existing in a commercial scale</li>
+                                                                                <li>At least 50.000 beneficiaries directly impacted by the solution</li>
+                                                                                <li>All applicants must have revenue and prior funding: they must have both, even if they are self-funded </li>
+                                                                                <li>Scale up teams need to dedicate at least 1 hour a month to the program and  attend physically the 2 main annual HealthTech summits in 2023</li>
+                                                                            </ol>
+                                                                        </ul>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+
+
+
                         </div>
                     </div>
 
                 </div>
-            </div>
+            </div><br>
+
+            
 
             <div class="mb-5 d-flex justify-content-center">
                 <a href="https://docs.google.com/forms/d/11-eGLae-AEL6dCK6vShAs4RP6SBQhOpqIay031BbRBI/prefill" target="_blank" class="btn btn-primary">Apply Now</a>
